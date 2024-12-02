445e469ede0b49bac61da542065ac2673d604be3
willyanne11 <willyannemayara5@gmail.com>
2024-12-02 20:28:02 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+let numero = parseFloat(prompt('Digite um número: '));
+
+if (numero < 0) {
+    
+    alert("Você digitou um número negativo");
+
+} else if(numero > 0){
+
+    alert("Você digitou um número positivo");
+
+} else {
+    
+    alert("Você digitou zero");
+}
+
